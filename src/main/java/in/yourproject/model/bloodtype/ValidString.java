2eed72709fe153b170e0a96c4d6f96421aab0926
Hemalatha G <hemsdg123@gmail.com>
2021-07-14 12:09:13 +0530

    Added project files

---
+++ src/main/java/in/yourproject/model/bloodtype/ValidString.java
@@ -0,0 +1,37 @@
+package in.yourproject.model.bloodtype;
+
+public class ValidString {
+
+
+	public static void main(String[] args) {
+
+		//input
+		String bloodType = "O+";
+		String bloodId = "";
+
+		
+		try {
+			
+			// Start - Validation
+			if (bloodType == null || bloodType.trim().equals("")) {
+				throw new Exception("Invalid bloodtype"); // Jump to catch block
+			}
+			else if(bloodId == null || bloodId.trim().equals("")) {
+				throw new Exception("Invalid bloodid.");
+			}
+
+			// End Validation
+			
+			// StudentDAO.save(name); 
+
+			System.out.println("Success");
+		} catch (Exception e) {
+			System.out.println("Catch");
+			String errorMessage = e.getMessage();
+			System.out.println("errorMessage:" + errorMessage);
+			e.printStackTrace();
+		}
+	}
+
+}
+
