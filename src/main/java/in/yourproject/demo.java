eba9b90ee89f883c38dd8140ae4ff1bb18922a89
Benzy W <benzywilson24@gmail.com>
2021-07-14 12:15:26 +0530

    Added validator

---
+++ src/main/java/in/yourproject/demo.java
@@ -0,0 +1,53 @@
+package in.yourproject;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class demo{
+
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
+		
+		
+		String driverClassName = "oracle.jdbc.driver.OracleDriver";		
+		String url = "jdbc:oracle:thin:@192.168.0.20:1521:DBEBS12";		
+		String username = "apps";		
+		String password = "apps";
+		
+		//Step 1: Load the driver
+		Class.forName(driverClassName);
+		
+		//Step 2: Connection
+		
+		Connection connection = DriverManager.getConnection(url, username,password);
+		//connection.setAutoCommit(false);//default true 
+		
+		System.out.println(connection);
+		
+		String stud_name = "Latha";
+		//String sql = "insert into test_students(name) values ('" + name + "')";
+		//String sql = "insert into test_student_v1(student_id,stud_name) values (XX_test.nextval,?)";
+		String sql = "delete from test_student_v1 where stud_name = ?";
+		System.out.println(sql);
+		
+		
+		PreparedStatement pst = connection.prepareStatement(sql);
+		pst.setString(1, stud_name);
+		int rows = pst.executeUpdate();
+		
+		//connection.commit();
+		
+		//connection.rollback();
+		
+		pst.close();
+		
+		connection.close();
+		
+		
+		System.out.println("No of rows inserted :" + rows);
+		
+
+	}
+
+}
