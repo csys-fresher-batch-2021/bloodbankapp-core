809b2befa897aa05e2240c21af4d29c9e7279809
Hemalatha G <hemsdg123@gmail.com>
2021-07-15 07:50:19 +0530

    updated connection util

--- src/main/java/com/hemalatha/util/ConnectionUtilTest.java
+++ src/main/java/com/hemalatha/util/ConnectionUtilTest.java
@@ -5,55 +5,65 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import com.bloodtype.exception.DBException;
 
 	public class ConnectionUtilTest {
 
-		public static void main(String[] args) throws SQLException, ClassNotFoundException {
-			
-			
+		public static Connection getConnection() throws DBException {
 			String driverClassName = "oracle.jdbc.driver.OracleDriver";		
-			String url = "jdbc:oracle:thin:@192.168.0.20:1521:DBEBS12";		
+			String url = "jdbc:oracle:thin:@192.168.0.20:1521:DBEBS12";			
 			String username = "apps";		
 			String password = "apps";
 			
-			//Step 1: Load the driver
-			Class.forName(driverClassName);
+			Connection connection = null;
+			try {
+				//Step 1: Load the driver
+				Class.forName(driverClassName);
+				
+				//Step 2: Connection			
+				connection = DriverManager.getConnection(url, username,password);
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+				throw new DBException(e,"Unable to load the db driver class");
+			} catch (SQLException e) {
+				e.printStackTrace();
+				throw new DBException(e,"Unable to connect database");
+			}
 			
-			//Step 2: Connection
-			
-			Connection connection = DriverManager.getConnection(url, username,password);
-			//connection.setAutoCommit(false);//default true 
-			
-			System.out.println(connection);
-			
-			String stud_name = "Latha";
-			//String sql = "insert into test_students(name) values ('" + name + "')";
-			//String sql = "insert into test_student_v1(student_id,stud_name) values (XX_test.nextval,?)";
-			String sql = "delete from test_student_v1 where stud_name = ?";
-			System.out.println(sql);
-			
-			
-			PreparedStatement pst = connection.prepareStatement(sql);
-			pst.setString(1, stud_name);
-			int rows = pst.executeUpdate();
-			
-			//connection.commit();
-			
-			//connection.rollback();
-			
-			pst.close();
-			
-			connection.close();
-			
-			
-			System.out.println("No of rows inserted :" + rows);
-			
+			System.out.println(connection);		
 
+			return connection;
 		}
+		
+		
+		public static void close(PreparedStatement pst, Connection connection) {
+			try {
+				if(pst !=null) {
+					pst.close();
+				}
+				if(connection !=null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				//suppress exception
+			}
+		}
+		
+		public static void close(PreparedStatement pst, ResultSet rs, Connection connection) {
+			try {
+				if(rs != null) {
+					rs.close();
+				}
+				if(pst !=null) {
+					pst.close();
+				}
+				if(connection !=null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				//suppress exception
+			}
+		}
+	
 
-	}
-
-
-
+}
