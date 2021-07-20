c48570ccea2332bb2e2aa7673f59d82ea7598845
hemalatha3001 <87056056+hemalatha3001@users.noreply.github.com>
2021-07-20 15:57:39 +0530

    Update BloodTypeDetailDAO.java

--- src/main/java/in/yourproject/bloodtypedetail/dao/BloodTypeDetailDAO.java
+++ src/main/java/in/yourproject/bloodtypedetail/dao/BloodTypeDetailDAO.java
@@ -5,57 +5,17 @@ package in.yourproject.bloodtypedetail.dao;
 	import in.yourproject.model.bloodtype.BloodTypeDetail;
 
 	public interface BloodTypeDetailDAO {
-		//save( passing too many things as arguments ) - BAD Practice
-				public static void save(String bloodId, String bloodType) {
-					
-				}
-				
-				// save( BAG bag)  
-				public static void save(BloodTypeDetail bloodTypeDetail) {
-					System.out.println(bloodTypeDetail);
-					String sql = "insert into bloodTypeDetail (bloodId,bloodType) values (" + bloodTypeDetail.bloodId + ",'" + bloodTypeDetail.bloodType +"')";
-					
-					//Check whether query is formed correctly with the data
-					System.out.println(sql);
-					
-				}
-				
-				public static ArrayList<BloodTypeDetail> findAll() {
-					String sql ="select bloodType from bloodtypedetail";
-					System.out.println(sql);
-					
-					//Empty Bag
-					ArrayList<BloodTypeDetail> bloodTypes = new ArrayList<BloodTypeDetail>();
-					
-				//Temporary =>  JDBC ==> Start ( 5 lines of code => we will fetch data from oracle tables )
-					BloodTypeDetail bloodtype1 = new BloodTypeDetail("BL01","O+");
-					BloodTypeDetail bloodtype2 = new BloodTypeDetail("BL02","O-");
-					
+	
+			ArrayList<BloodTypeDetail> findAll() throws Exception;
+			
+			BloodTypeDetail findOne(String bloodType) throws Exception;
+		}
 
-					bloodTypes.add(bloodtype1); // Add items to bag
-					bloodTypes.add(bloodtype2);// Add items to bag
-					
-					//Temporary =>  JDBC ==> End				
-					return bloodTypes; //return type
-					
-					
-				}
-				
-				public static BloodTypeDetail findOne(String bloodId) {
-					String sql ="select bloodId,bloodType from BloodTypeDetail where bloodId =" + bloodId;
-					System.out.println(sql);	
-					
-					//Temporary =>  JDBC ==> Start ( 5 lines of code => we will fetch data from oracle tables )
-					BloodTypeDetail bloodtype1 = new BloodTypeDetail("BL01","O+");		
-					//Temporary =>  JDBC ==> End				
-					
-					return bloodtype1;
-					
-					
-				}
-						
+		
+		
 
-			}
+		
+
 
 
 		
