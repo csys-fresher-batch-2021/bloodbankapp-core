d646383a60cf98db18d327eb77b49feb736b207c
hemalatha3001 <87056056+hemalatha3001@users.noreply.github.com>
2021-07-20 15:19:54 +0530

    Update DeleteDonorDAO.java

--- src/main/java/in/yourproject/DeleteDonorDAO.java
+++ src/main/java/in/yourproject/DeleteDonorDAO.java
@@ -1,59 +1,9 @@
 package in.yourproject;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.time.LocalDate;
 
+import in.yourproject.DonorDetails;
 
-public class DeleteDonorDAO {
+public interface DeleteDonorDAO {
 
-
-	public static void main(String[] args) throws SQLException, ClassNotFoundException {
-		// TODO Auto-generated method stub
-
-		String driverClassName = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@192.168.0.20:1521:DBEBS12";
-		String username = "apps";
-		String password = "apps";
-
-		// Step 1: Load the driver
-		Class.forName(driverClassName);
-
-		// Step 2: Connection
-
-		Connection connection = DriverManager.getConnection(url, username, password);
-		// connection.setAutoCommit(false);//default true
-
-		System.out.println(connection);
-
-		// String name = ;
-		   int active=1;
-		
-		
-			String sql = "delete from donor_hem where active < ?";
-			System.out.println(sql);
-
-			PreparedStatement pst = connection.prepareStatement(sql);
-			pst.setInt(1, active);
-			
-			int rows = pst.executeUpdate();
-			pst.close();
-
-			connection.close();
-
-			System.out.println("No of rows deleted :" + rows);
+	boolean delete(int active) throws Exception;
 	
-		}
-		
-		
-
-		// connection.commit();
-
-		// connection.rollback();
-
-		
-
-	}
-
+}
