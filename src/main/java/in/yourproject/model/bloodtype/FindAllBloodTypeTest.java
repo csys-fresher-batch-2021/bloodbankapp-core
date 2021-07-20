7e40a1f7b57d35678a4692cc593c59253159e96e
hemalatha3001 <87056056+hemalatha3001@users.noreply.github.com>
2021-07-20 19:05:14 +0530

    Update FindAllBloodTypeTest.java

--- src/main/java/in/yourproject/model/bloodtype/FindAllBloodTypeTest.java
+++ src/main/java/in/yourproject/model/bloodtype/FindAllBloodTypeTest.java
@@ -1,17 +1,16 @@
 package in.yourproject.model.bloodtype;
 import in.yourproject.bloodtypedetail.dao.BloodTypeDetailDAO;
 import in.yourproject.bloodtypedetail.dao.BloodTypeDAOImp;
-import in.yourproject.model.bloodtype.BloodTypeDetail;
-import java.util.ArrayList;
+import java.util.List;
 
 public class FindAllBloodTypeTest {
 	public static void main(String[] args) {
 
 		BloodTypeDetailDAO bloodTypeDAO = new BloodTypeDAOImp();
 		try {
-			ArrayList<BloodTypeDetail> bloodTypeList = bloodTypeDAO.findAll();
+			List<BloodTypeDetail> bloodTypeList = bloodTypeDAO.findAll();
 			for (BloodTypeDetail bloodType : bloodTypeList) {
-				System.out.println();
+				System.out.println(bloodType);
 			}
 		} catch (Exception e) {
 			String errorMessage = e.getMessage();
